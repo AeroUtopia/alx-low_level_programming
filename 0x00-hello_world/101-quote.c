3be5df5aa31ac4c3be9307ff285de67cb6b34a0e
Semachew Fasika <eth.saf.1992@gmail.com>
2022-04-07 15:11:09 +0300

    101-quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,17 @@
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
+}
 }
