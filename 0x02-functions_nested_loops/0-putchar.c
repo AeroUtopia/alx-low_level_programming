42099eca685b625d7868bafd65969305f68a407d
Semachew Fasika <eth.saf.1992@gmail.com>
2022-04-12 21:28:50 +0300

    0-putchar c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,14 @@
-/*
- * File: 0_putchar.c
- * Auth: Semachew
- *
- */
-
-#include "_putchar.h"
+#include "holberton.h"
+#include <unistd.h>
 
 /**
- * main - Prints "_putchar" followed by a new line.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
