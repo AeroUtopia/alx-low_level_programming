9f128b31f61081fa88cefc3832ebb4a0689faca7
Semachew Fasika <eth.saf.1992@gmail.com>
2022-04-13 00:01:40 +0300

    102-fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,46 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  * main - print the first 98 fibonacci numbers.
+  * main - print the first 50 fibonacci numbers.
   * Return: Nothing.
   */
 int main(void)
 {
-	int count;
-	unsigned long i, j, k;
-	unsigned long m, n, p, carry;
+	unsigned long count, i, j, k;
 
-	count = 0;
 	i = 0;
 	j = 1;
-	for (count = 1; count <= 91; count++)
+	for (count = 0; count < 50; count++)
 	{
 		k = i + j;
 		i = j;
 		j = k;
-		printf("%lu, ", k);
-	}
-	m = i % 1000;
-	i = i / 1000;
-	n = j % 1000;
-	j = j / 1000;
-	while (count <= 98)
-	{
-		carry = (m + n) / 1000;
-		p = (m + n) - carry * 1000;
-		k = (i + j) + carry;
-		m = n;
-		n = p;
-		i = j;
-		j = k;
-		if (p >= 100)
-			printf("%lu%lu", k, p);
+		printf("%lu", k);
+		if (count == 49)
+			putchar('\n');
 		else
-			printf("%lu0%lu", k, p);
-		if (count != 98)
+		{
 			printf(", ");
-		count++;
+		}
 	}
-	putchar('\n');
 	return (0);
 }
