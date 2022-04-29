28d4e376fb01cbe12979027baca315651d4b8008
Semachew Fasika <eth.saf.1992@gmail.com>
2022-04-29 21:35:43 +0300

    100-change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,71 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <stdbool.h>
 
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
- */
-int coinConverter(int i)
-{
-	int count = 0;
-
-	while (i != 0)
-	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-		else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ * main - prints buffer in hexa
+ * @argc: he number of command line arguments
+ * @argv: An array containing the program command line arguments.
+ *
+ * Return: Nothing.
  */
 int main(int argc, char *argv[])
 {
-	int i, coin;
+	int j = 0, i = 1, coins, change = 0, a;
+	int cents[5] = {25, 10, 5, 2, 1};
 
-	coin = 0;
+	if (argc == 2)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] == '-')
+			{
+				printf("0\n");
+				return (0);
+			}
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		coins = atoi(argv[i]);
+		for (a = 0; a < 5; a++)
+		{
 
-	if (argc != 2)
+			while (coins >= cents[a])
+			{
+				coins -= cents[a];
+				change += 1;
+			}
+		}
+		printf("%d\n", change);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	i = atoi(argv[1]);
-
-	if (i < 0)
-		printf("0\n");
-	else
-	{
-		coin = coinConverter(i);
-
-		printf("%d\n", coin);
-	}
-
 	return (0);
 }
