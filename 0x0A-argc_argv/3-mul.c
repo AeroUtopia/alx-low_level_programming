07277f13e40e80b23da8082f4f119e1c6b422bf9
Semachew Fasika <eth.saf.1992@gmail.com>
2022-04-29 21:22:47 +0300

    3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
  * main - Program that takes first two integer arguments and prints the product
@@ -25,5 +25,8 @@ int main(int argc, char *argv[])
 		for (i = 1; i < 3; i++)
 			j *= atoi(argv[i]);
 
+		printf("%d\n", j);
 	}
+
+	return (0);
 }
