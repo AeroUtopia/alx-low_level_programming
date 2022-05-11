53d3754258a1c22bb87d0af5b4fa7860eba60728
Semachew Fasika <eth.saf.1992@gmail.com>
2022-05-11 20:08:18 +0300

    main function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,10 @@
-#include <stdio.h>
 /**
-* print_name - function that prints a name
-*@name: name to print
-*@f: function to print name
-*Return: Nothing
-*/
+ * print_name -  prints a name.
+ * @name: - name to print
+ * @f: - function
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	void (*ptofunct)(char *);
-
-	if (name == NULL || f == NULL)
-		return;
-
-	ptofunct = f;
-	ptofunct(name);
+	if (name && f)
+		f(name);
 }
