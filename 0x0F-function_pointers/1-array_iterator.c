75f3f4796d6d0acb853af8488532a13c3159c143
Semachew Fasika <eth.saf.1992@gmail.com>
2022-05-12 19:48:28 +0300

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,19 +1,23 @@
-#include "main.h"
-#include <stdlib.h>
+#include "function_pointers.h"
 
 /**
- * array_iterator - prints a name.
- * @array: - array
- * @size: - size of string
- * @action: - function compare
- */
+  * array_iterator - ...
+  * @array: ...
+  * @size: ...
+  * @action: ...
+  *
+  * Return: ...
+  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	if (array && size && action)
+	if (array != NULL && action != NULL && size > 0)
 	{
-		for (i = 0; i < size; i++)
+		while (i < size)
+		{
 			action(array[i]);
+			i++;
+		}
 	}
 }
